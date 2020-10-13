987de9d090875ed908951151d36b46ece7ef3fcc
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-10-13 16:18:09 +0900

    Made initial toolchain

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.html";
+
+ReactDOM.render(
+  <div>Hello, React world!</div>,
+  document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: path.join(__dirname, "src", "index.tsx"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.html$/,
+        loader: "file-loader",
+        options: { name: "[name].[ext]" },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+};
