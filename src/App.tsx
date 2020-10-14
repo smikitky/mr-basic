e1605c6c37746ac486f34a29997a477a1b93531f
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-10-14 21:03:32 +0900

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import AppBar from '@material-ui/core/AppBar';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import Toolbar from '@material-ui/core/Toolbar';
 import HelpIcon from '@material-ui/icons/HelpOutline';
+import Typography from '@material-ui/core/Typography';
 import React, { FC } from 'react';
 import {
   BrowserRouter,
@@ -44,6 +46,9 @@ const App: FC = props => {
                 {route.comp}
               </Route>
             ))}
+            <Route exact path="/">
+              <Home />
+            </Route>
           </Switch>
         </div>
       </div>
@@ -74,6 +79,28 @@ const MainNav: FC = props => {
   );
 };
 
+const Home = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Typography variant="h1">MRI Basics</Typography>
+      <div className={classes.homeMenu}>
+        {routes.map(route => (
+          <Button
+            variant="contained"
+            color="primary"
+            component={Link}
+            to={route.path}
+            size="large"
+          >
+            {route.title}
+          </Button>
+        ))}
+      </div>
+    </>
+  );
+};
+
 const useStyles = makeStyles(theme => ({
   root: {
     height: '100%',
@@ -87,6 +114,12 @@ const useStyles = makeStyles(theme => ({
   },
   grow: {
     flexGrow: 1
+  },
+  homeMenu: {
+    maxWidth: '300px',
+    display: 'flex',
+    gap: '10px',
+    flexDirection: 'column'
   }
 }));
 
