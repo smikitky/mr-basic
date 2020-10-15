ce74944646a281e8822487a1911e40316981ace7
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-10-15 09:04:41 +0900

    Add k-space visualizer

--- src/Wave2D.tsx
+++ src/Wave2D.tsx
@@ -11,38 +11,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const N = 32;
 
-const useStyles = makeStyles(theme => ({
-  root: {},
-  waveArea: {
-    display: 'grid',
-    width: 'auto',
-    gridTemplateAreas: `'v c' 'a h'`,
-    gridTemplateRows: '1100px auto',
-    gridTemplateColumns: 'auto 1100px',
-    margin: '0 auto',
-    justifyContent: 'center',
-    [theme.breakpoints.down('md')]: {
-      gridTemplateRows: '800px auto',
-      gridTemplateColumns: 'auto 800px'
-    },
-    [theme.breakpoints.down('sm')]: {
-      gridTemplateRows: '550px auto',
-      gridTemplateColumns: 'auto 550px'
-    }
-  },
-  canvas: { gridArea: 'c', width: '100%', height: '100%' },
-  gradientX: { gridArea: 'h' },
-  gradientY: { gridArea: 'v' },
-  menu: {
-    padding: '15px',
-    gridArea: 'm',
-    display: 'flex',
-    flexFlow: 'row',
-    gap: '5px',
-    marginBottom: '10px'
-  }
-}));
-
 const phaseToColor = (phase: number) => {
   const hex = (
     Math.floor(Math.max(0, -Math.sin(phase)) * 256) * 256 * 256 +
@@ -102,6 +70,7 @@ const Wave2D: FC = props => {
   const [gradientY, setGradientY] = useState(0);
   const [pause, setPause] = useState(false);
   const [protonType, setProtonType] = useState<'circle' | 'square'>('circle');
+  const [k, setK] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
   const phasesRef = useRef<number[][]>();
   if (!phasesRef.current) {
     phasesRef.current = new Array(N).fill(0).map(_ => new Array(N).fill(0));
@@ -132,6 +101,10 @@ const Wave2D: FC = props => {
             300;
         }
       }
+      setK({
+        x: (phases[N - 1][0] - phases[0][0]) / (2 * Math.PI),
+        y: (phases[0][0] - phases[0][N - 1]) / (2 * Math.PI)
+      });
 
       ctx.clearRect(0, 0, width, height);
       const cellSize = width / N;
@@ -157,8 +130,17 @@ const Wave2D: FC = props => {
     };
   }, [gradientX, gradientY, pause, protonType]);
 
+  const rephase = (kx: number, ky: number) => {
+    for (let x = 0; x < N; x++) {
+      for (let y = 0; y < N; y++) {
+        phases[x][y] =
+          (x * kx * 2 * Math.PI) / (N - 1) - (y * ky * 2 * Math.PI) / (N - 1);
+      }
+    }
+  };
+
   const handleRephaseClick = () => {
-    phases.forEach((_, i) => (phases[i] = new Array(N).fill(0)));
+    rephase(0, 0);
   };
 
   const handleProtonTypeChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -203,6 +185,7 @@ const Wave2D: FC = props => {
             />
           }
         />
+        <KSpace x={k.x} y={k.y} onKSpaceChange={rephase} />
       </Card>
       <div className={classes.waveArea}>
         <canvas
@@ -236,5 +219,98 @@ const Wave2D: FC = props => {
     </div>
   );
 };
+
+const KSpace: FC<{
+  x: number;
+  y: number;
+  onKSpaceChange: (x: number, y: number) => void;
+}> = props => {
+  const classes = useStyles();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [down, setDown] = useState(false);
+  const { x = 0, y = 0, onKSpaceChange } = props;
+
+  useEffect(() => {
+    const canvas = canvasRef.current!;
+    const width = canvas.width;
+    const height = canvas.height;
+    const ctx = canvas.getContext('2d')!;
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.moveTo(0, height / 2);
+    ctx.lineTo(width, height / 2);
+    ctx.moveTo(width / 2, 0);
+    ctx.lineTo(width / 2, height);
+    ctx.stroke();
+    ctx.fillStyle = '#ff0000';
+    ctx.beginPath();
+    ctx.ellipse(
+      x * 5 + width / 2,
+      -y * 5 + height / 2,
+      5,
+      5,
+      0,
+      0,
+      Math.PI * 2
+    );
+    ctx.fill();
+  }, [x, y]);
+
+  const handlePointerMove = (ev: React.PointerEvent) => {
+    if (!down) return;
+    const rect = canvasRef.current!.getBoundingClientRect();
+    const clickX = ev.clientX - rect.x;
+    const clickY = ev.clientY - rect.y;
+    const kx = Math.round((clickX - rect.width / 2) / 5);
+    const ky = Math.round(-(clickY - rect.height / 2) / 5);
+    onKSpaceChange(kx, ky);
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      className={classes.kSpace}
+      width={100}
+      height={100}
+      onPointerDown={() => setDown(true)}
+      onPointerMove={handlePointerMove}
+      onPointerUp={() => setDown(false)}
+    />
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  waveArea: {
+    display: 'grid',
+    width: 'auto',
+    gridTemplateAreas: `'v c' 'a h'`,
+    gridTemplateRows: 'min(80vw, 80vh) auto',
+    gridTemplateColumns: 'auto min(80vw, 80vh)',
+    margin: '0 auto',
+    justifyContent: 'center'
+  },
+  canvas: {
+    cursor: 'pointer',
+    gridArea: 'c',
+    width: 'min(80vw, 80vh)',
+    height: 'min(80vw, 80vh)'
+  },
+  gradientX: { gridArea: 'h' },
+  gradientY: { gridArea: 'v' },
+  menu: {
+    gridArea: 'm',
+    padding: '15px',
+    display: 'flex',
+    flexFlow: 'row',
+    alignItems: 'center',
+    gap: '5px',
+    marginBottom: '10px'
+  },
+  kSpace: {
+    boxShadow: '0 0 8px 2px silver'
+  }
+}));
 
 export default Wave2D;
